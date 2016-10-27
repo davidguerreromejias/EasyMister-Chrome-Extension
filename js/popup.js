9b2e53ac12be0e2f057654b1de73fef4ea87a0eb
Eloi Pardo <eloipr94@gmail.com>
2016-10-27 16:45:40 +0200

    .idea borrat

--- js/popup.js
+++ js/popup.js
@@ -1,13 +1,3 @@
-$('.btn-simple').each(function(i, obj) {
-    var money = 1000000;
-    var price = $(this).attr('data-price');
-    if (money < price){
-      $(this).css("background-color","red");
-      $(this).css("font-style","bold");
-      $(this).css("color","black");
-    }
-    else{
-      $(this).append( "<p>Compra</p>" );
-      $(this).css("background-color","rgba(0, 181, 20, 0.64)");
-    }
+$("#checkPage").click(function () {
+    alert("hola");
 });
